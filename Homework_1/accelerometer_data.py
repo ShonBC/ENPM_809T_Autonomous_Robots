9e0a9d0d4559dc3aa8a7128ca2e91e50d097ec0d
ShonBC <cortesshon@gmail.com>
2022-01-31 16:10:23 -0800

    init

---
+++ Homework_1/accelerometer_data.py
@@ -0,0 +1,59 @@
+from cProfile import label
+from turtle import color
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+def PlotIMUData(imu_angle, incrament):
+
+    # Generate x-axis data
+    x = np.linspace(1, imu_angle.size, imu_angle.size)
+
+    # Raw IMU Data Plotting
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    # plt.plot(x, imu_angle, 'k')
+    ax.plot(x, imu_angle, color='red', linewidth=.5, label='IMU_Data')
+
+    # Moving Average Plotting
+    mavg = np.array(MovingAvg(imu_angle, incrament))
+    std_dev = np.std(mavg)
+    mean_data = mavg.mean()
+    x1 = np.linspace(1, imu_angle.size, mavg.size)
+    ax.plot(x1, mavg, color='blue', linewidth=.5, label=f'Moving Average [{incrament}]')
+    ax.text(1,1,
+            f'Mean: {mean_data}, \nStandard Deviation: {std_dev}',
+            style='italic')
+
+    ax.legend(loc='best')
+    ax.set(title='IMU Data History',
+    ylabel='IMU Angle Reading [Degrees]',
+    xlabel='Data Entry Number')
+
+    plt.show()
+    
+    
+
+def MovingAvg(imu_angle, incrament = 10):
+    
+    avg_list = []
+    ang_list = []
+    for i in range(len(imu_angle)):
+        ang_list.append(imu_angle[i])
+        if len(ang_list) >= incrament:
+            avg_list.append(sum(ang_list) / incrament)
+            ang_list = []
+
+    return avg_list
+
+if __name__ == '__main__':
+
+    # Load imu angular data in the 5th column of the data file
+    imu_angle = np.loadtxt('Homework_1/imudata.txt', usecols= 4)
+
+    PlotIMUData(imu_angle, 2)
+    # PlotIMUData(imu_angle, 4)
+    # PlotIMUData(imu_angle, 8)
+    # PlotIMUData(imu_angle, 16)
+    # PlotIMUData(imu_angle, 64)
+    # PlotIMUData(imu_angle, 128)
