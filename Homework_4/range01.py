ab7f6a8743209092ab04015fe68050b2a01a44a2
ShonBC <cortesshon@gmail.com>
2022-02-23 19:34:52 -0800

    init

---
+++ Homework_4/range01.py
@@ -0,0 +1,41 @@
+import RPi.GPIO as gpio
+import time
+
+# Define pin allocations
+trig = 16
+echo = 18
+
+def Distance():
+
+    # Assign inputs and outputs to RPi GPIO pins
+    gpio.setmode(gpio.BOARD)
+    gpio.setup(trig, gpio.OUT)
+    gpio.setup(echo, gpio.IN)
+
+    # Ensure output has no value
+    gpio.output(trig, False)
+    time.sleep(0.01)
+
+    # Generate trigger pulse
+    gpio.output(trig, True)
+    time.sleep(0.00001)
+    gpio.output(False)
+
+    # Generate echo time signal
+    while gpio.input(echo) == 0:
+        pulse_start = time.time()
+
+    while gpio.input(echo) == 1:
+        pulse_end = time.time()
+
+    pulse_duration = pulse_end - pulse_start
+
+    # Convert time to distance in [cm] using speed of sound
+    distance = pulse_duration * 17150
+    distance = round(distance, 2)
+
+    # Cleanup gpio pins & return distance estimate
+    gpio.cleanup()
+    return distance
+
+print(f'Distance: {Distance()} cm')
