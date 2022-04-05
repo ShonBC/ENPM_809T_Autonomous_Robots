ef48a4870698df6e18209f528020821be80e7277
ShonBC <cortesshon@gmail.com>
2022-04-04 17:27:33 -0700

    init

---
+++ Homework_8/imu01.py
@@ -0,0 +1,36 @@
+
+import serial
+
+# Identify serial connection on RPI
+ser = serial.Serial('/dev/ttyUSB0', 9600)
+
+count = 0
+
+while True:
+
+    if(ser.in_waiting > 0):
+
+        count +=1
+
+        # Read serial stream
+        line = ser. readline()
+        print(line)
+
+        # Avoid first n-lines of serial info
+        if count > 10:
+
+            # Strip serial stream of extra characters
+
+            line = line.rstrip().lstrip()
+            print(line)
+
+            line = str(line)
+            line = line.strip("'")
+            line = line.strip("b'")
+
+            print(line)
+
+            # Return float
+            line = float(line)
+
+            print(f'line \n')
