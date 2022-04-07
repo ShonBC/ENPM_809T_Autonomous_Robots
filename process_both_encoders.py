cd3d45ccd54b82f961bb9e34ee2499ffcfadce9f
ShonBC <cortesshon@gmail.com>
2022-04-07 13:00:02 -0700

    Move to use on future assignments

--- process_both_encoders.py
+++ process_both_encoders.py
@@ -5,18 +5,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 if __name__ == '__main__':
+
+    title = 'Motor Encoder Analysis Driving in Reverse 1 meter'
     
     # Load imu angular data in the 5th column of the data file
-    # BRstates = np.loadtxt('Homework_6/docs/BRencoderstates.txt')
-    # FLstates = np.loadtxt('Homework_6/docs/FLencoderstates.txt')
-    BRstates = np.loadtxt('Homework_6/docs/BRencoderstates_both.txt')
-    FLstates = np.loadtxt('Homework_6/docs/FLencoderstates_both.txt')
+    BRstates = np.loadtxt('Homweork_7/docs/Reverse_BRencoderstates.txt')
+    FLstates = np.loadtxt('Homweork_7/docs/Reverse_FLencoderstates.txt')
     x1 = np.arange(0, BRstates.size, step=1)
     x2 = np.arange(0, FLstates.size, step=1)
 
     # Time delta Data Plotting on same figure
     fig, axs = plt.subplots(2)
-    fig.suptitle('Motor Encoder Analysis')
+    fig.suptitle(title)
 
     axs[0].plot(x1, BRstates)
     axs[0].set(ylabel='Back Right Encoder')
